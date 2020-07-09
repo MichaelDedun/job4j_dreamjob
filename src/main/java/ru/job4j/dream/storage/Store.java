0eaf714f7718553221f5a2a536d81d85626c160b
Michael Dedun <michael.dedun@mail.ru>
2020-07-09 13:50:41 +0600

    Задание [276522#317740]

--- src/main/java/ru/job4j/dream/storage/Store.java
+++ src/main/java/ru/job4j/dream/storage/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.storage;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.time.LocalDate;
@@ -14,10 +15,15 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "без опыта работы", LocalDate.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "опыт работы 2 года", LocalDate.of(2020, Month.APRIL,23)));
+        posts.put(2, new Post(2, "Middle Java Job", "опыт работы 2 года", LocalDate.of(2020, Month.APRIL, 23)));
         posts.put(3, new Post(3, "Senior Java Job", "опыт работы 5 лет", LocalDate.of(2019, Month.DECEMBER, 22)));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
@@ -26,6 +32,10 @@ public class Store {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 
 }

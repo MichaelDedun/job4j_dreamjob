dd9a7c6b38335df2c4a12b37c7c8d7347d823c3c
Michael Dedun <michael.dedun@mail.ru>
2021-02-24 21:19:31 +0300

    5. Реализация интерфейса[2519#317727]

---
+++ src/main/java/ru/job4j/dream/model/City.java
@@ -0,0 +1,53 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+public class City {
+    private Integer id;
+    private String name;
+
+    public City(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public City setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public City setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(id, city.id) &&
+                Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
